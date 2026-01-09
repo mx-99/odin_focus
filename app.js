d607076b15c02562e6f330fbb407f2ae9605e519
mx-99 <125824465+mx-99@users.noreply.github.com>
2026-01-09 20:17:42 +0300

    Hide MDN nav, TOC, sidebars, and footer sections

---
+++ app.js
@@ -0,0 +1,9 @@
+document.querySelector(".navigation").style.display = "none";
+document.querySelector(".reference-layout__sidebar").style.display = "none";
+document.querySelector(".reference-layout__toc").style.display = "none";
+document.querySelector('section[aria-labelledby="see_also"]').style.display =
+  "none";
+document.querySelector('section[aria-labelledby="feedback"]').style.display =
+  "none";
+document.querySelector(".footer__mdn").style.display = "none";
+document.querySelector(".footer__mozilla").style.display = "none";
